7bcd6984a7be3c80d9f2e26414a45874b662d7ea
Simon Camphausen <simon_camp@gmx.de>
2017-03-10 11:47:44 +0100

    add first draft of odtwriter

---
+++ src/main/java/de/naju/ahlen/io/ODTWriter.java
@@ -0,0 +1,93 @@
+package de.naju.ahlen.io;
+
+import de.naju.ahlen.model.Area;
+import org.odftoolkit.odfdom.doc.OdfTextDocument;
+import org.odftoolkit.odfdom.dom.element.office.OfficeTextElement;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Writer implementation for ODT templates
+ * Created by simon on 10.03.17.
+ */
+public class ODTWriter implements Writer{
+
+    // TODO disable info logging from odfdom
+
+    private final String VARIABLE_NODE_NAME = "text:variable-set";
+    private final String VARIABLE_NODE_CONTENT = "text:name";
+
+    private OdfTextDocument docCashPayment;
+    private OdfTextDocument docDonation;
+    private OdfTextDocument docOperation;
+
+    /**
+     *
+     * @param fileCashPayment odt template for cash payments
+     * @param fileDonation odt template for donations
+     * @param fileOperation odt template for operations
+     */
+
+    public ODTWriter(File fileCashPayment, File fileDonation, File fileOperation) {
+        try {
+            docCashPayment = OdfTextDocument.loadDocument(fileCashPayment);
+            docDonation = OdfTextDocument.loadDocument(fileDonation);
+            docOperation = OdfTextDocument.loadDocument(fileOperation);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void write(Area area) {
+        writeVariables(docCashPayment);
+        writeVariables(docDonation);
+        writeVariables(docOperation);
+    }
+
+    private void writeVariables(OdfTextDocument doc) {
+        List<Node> variables = collectVariables(doc);
+
+        for (Node node : variables) {
+            System.out.println("variable_name: " + node.getAttributes().getNamedItem("text:name").getNodeValue());
+            System.out.println("variable_value: " + node.getChildNodes().item(0).getNodeValue());
+        }
+    }
+
+    private List<Node> collectVariables(OdfTextDocument doc) {
+        List<Node> vars = new ArrayList<>();
+
+        try {
+            OfficeTextElement contentRoot = doc.getContentRoot();
+            NodeList childNodes = contentRoot.getChildNodes();
+            vars = collectNodeVariables(childNodes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vars;
+    }
+
+    private List<Node> collectNodeVariables(NodeList childNodes) {
+        List<Node> vars = new ArrayList<>();
+
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+
+            if (item instanceof NodeList) {
+                List<Node> innerVars = collectNodeVariables(item.getChildNodes());
+                vars.addAll(innerVars);
+            }
+            if (item.getNodeName().equals(VARIABLE_NODE_NAME)) {
+                //String nodeValue = item.getAttributes().getNamedItem("text:name").getNodeValue();
+                //item.getChildNodes().item(0).setNodeValue("REPLACEMENT"+i);
+                vars.add(item);
+            }
+        }
+
+        return vars;
+    }
+}
---
+++ src/main/java/de/naju/ahlen/io/Writer.java
@@ -0,0 +1,12 @@
+package de.naju.ahlen.io;
+
+import de.naju.ahlen.model.Area;
+
+/**
+ * Write data from Area object to destination
+ * Created by simon on 10.03.17.
+ */
+public interface Writer {
+
+    void write(Area area);
+}
